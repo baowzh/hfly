059790bfae71387bef59a9a6cb8dd2d09eae2d2a
baowzh <imubwz@126.com>
2015-05-16 16:06:07 +0800

    旅游线路维护

--- admin/Lib/Model/LineModel.class.php
+++ admin/Lib/Model/LineModel.class.php
@@ -17,7 +17,7 @@ class LineModel extends Model {
         array("code", "require", "线路编号必须填写", 1),
         array("trip_days", "/^[1-9]\d*$/", "请输入一个有效且大于1的数字作为天数", 1),
         array("traffic", "require", "请填写往返交通方式", 1),
-        array("target", "check_call", "至少选择一个目的地地址", 1, 'callback', 3),
+       // array("target", "check_call", "至少选择一个目的地地址", 1, 'callback', 3),
         array("front_money", "/^[1-9]\d*$/", "请用0到100之间的数作为订金的百分比", 1),
         array("sort", "/^[0-9]\d*$/", "请输入一个有效数字作为排序字段", 1),
         array("linebelongto", "require", "线路归属地区必须填写", 1)
